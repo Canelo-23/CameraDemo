e40657a3dbfa4e2d90303f538715d9c0a0aed775
RANDY AGUILAR <rl106845@rlas-macair-20.local>
2018-12-04 11:13:14 -0600

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by RANDY AGUILAR on 12/4/18.
+//  Copyright © 2018 High School Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
